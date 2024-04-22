45797bbf83881c1d51025331edab95a2f619c27d
Everton Borges <evertonb@unochapeco.edu.br>
2024-04-22 10:37:27 -0300

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,3 +1,14 @@
 const upperCaseWords = (str) => str.replace(/^(.)|\s+(.)/g, (c) => c.toUpperCase());
 
 upperCaseWords('hello world'); // 'Hello World'
+
+
+function stringifySearch(search) {
+  return Object.entries(search)
+    .reduce(
+      (t, v) => `${t}${v[0]}=${encodeURIComponent(v[1])}&`,  Object.keys(search).length ? "?" : "" ).replace(/&$/, "");
+}
+
+const search = stringifySearch({name: "fatfish", age: 100});
+
+const link = `https://medium.com/${search}`; //"https://medium.com/?name=fatfish&age=100" 

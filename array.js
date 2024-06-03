7b08a9b53fae911cbd21cc7d0a990cb7c6e442d0
Everton Borges <evertonb@unochapeco.edu.br>
2024-06-03 02:31:48 -0300

    Update array.js

--- array.js
+++ array.js
@@ -2,6 +2,9 @@
 const a = [1, 2, "b", 0, {}, "", NaN, 3, undefined, null, 5, []];
 const b = a.filter(Boolean);  //[1, 2, "b", {}, 3, 5, []];
 
+//Generate array
+const arr = [...Array(10).keys()] //[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
 //Remove duplicate values in an array
 const removeDuplicates = (arr) => [...new Set(arr)];
 console.log(removeDuplicates([1, 2, 2, 3, 3, 4, 4, 5, 5, 6]));  // [1, 2, 3, 4, 5, 6]
@@ -33,3 +36,13 @@ while (index < values.length)
 chunks = [[3, 4, 5, 2, 6], [3, 2, 25, 5, 2], [45, 7]] 
 values = [3, 4, 5, 2, 6, 3, 2, 25, 5, 2, 45, 7]
 */
+
+//Chunk 3 lazy loading
+function* lazyLoadArray(data, chunkSize = 5) {
+  for (let i = 0; i < data.length; i += chunkSize)
+    yield data.slice(i, i + chunkSize);
+}
+
+const lazyLoader = lazyLoadArray([3, 4, 5, 2, 6, 3, 2, 25, 5, 2, 45, 7]);
+for (const chunk of lazyLoader)
+    console.log(chunk);
